027777eefc73a3b954e677a1cbfb8768c4b74791
210-reverof <sheltonwon@gmail.com>
2023-12-21 04:01:56 +0900

    test: 중복 이메일 예외 처리 테스트

--- src/user-service/src/test/java/com/boilerplate/userservice/user/application/UserServiceTest.java
+++ src/user-service/src/test/java/com/boilerplate/userservice/user/application/UserServiceTest.java
@@ -1,9 +1,11 @@
 package com.boilerplate.userservice.user.application;
 
 import com.boilerplate.userservice.user.dto.request.UserJoinRequest;
+import com.boilerplate.userservice.user.exception.DuplicateEmailException;
 import com.boilerplate.userservice.user.persistence.UserRepository;
 import com.boilerplate.userservice.user.persistence.domain.Gender;
 import com.boilerplate.userservice.user.persistence.domain.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,16 +29,32 @@ class UserServiceTest {
     @Test
     @DisplayName("회원을 추가한다.")
     void 회원을_추가한다() {
-        // Given
+        // given
         UserJoinRequest userJoinRequest = new UserJoinRequest(
                 "test@example.com", "password123", "testuser", "male", 25
         );
         when(passwordEncoder.encode("password123")).thenReturn("hashedPassword123");
 
-        // When
+        // when
         userService.join(userJoinRequest);
 
-        // Then
+        // then
         verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("중복된 이메일은 예외를 발생한다.")
+    void 중복된_이메일은_예외를_발생한다() {
+        // given
+        UserJoinRequest userJoinRequest = new UserJoinRequest(
+                "test@example.com", "password123", "testuser", "male", 25
+        );
+        when(userRepository.existsByEmail("test@example.com")).thenReturn(true);
+
+        // when
+        // then
+        Assertions.assertThrows(DuplicateEmailException.class, () -> {
+            userService.join(userJoinRequest);
+        }, "중복된 이메일이 예외를 발생");
     }
 }
