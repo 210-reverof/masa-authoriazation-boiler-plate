2069539b42882436583d4189549bb77151abba7c
210-reverof <sheltonwon@gmail.com>
2023-12-21 00:55:58 +0900

    docs: user 생성 스크립트 수정

--- scripts/won_user_script.sql
+++ scripts/won_user_script.sql
@@ -24,6 +24,9 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) DEFAULT NULL,
+  `deleted_at` datetime(6) DEFAULT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
   `age` int DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `gender` enum('FEMALE','MALE') DEFAULT NULL,
@@ -52,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-21  0:03:42
+-- Dump completed on 2023-12-21  0:50:19
