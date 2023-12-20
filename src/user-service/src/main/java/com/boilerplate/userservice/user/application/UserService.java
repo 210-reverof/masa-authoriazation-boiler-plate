5e73de98bb373347fe48f8daa80452dfcf5939b4
210-reverof <sheltonwon@gmail.com>
2023-12-21 03:07:50 +0900

    feat: 회원가입 비지니스 로직 추가

---
+++ src/user-service/src/main/java/com/boilerplate/userservice/user/application/UserService.java
@@ -0,0 +1,31 @@
+package com.boilerplate.userservice.user.application;
+
+import com.boilerplate.userservice.user.dto.request.UserJoinRequest;
+import com.boilerplate.userservice.user.persistence.UserRepository;
+import com.boilerplate.userservice.user.persistence.domain.Gender;
+import com.boilerplate.userservice.user.persistence.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void join(UserJoinRequest userJoinRequest) {
+        User user = convertToUser(userJoinRequest);
+
+        // TODO: 이메일 중복 예외처리
+        userRepository.save(user);
+    }
+
+    private User convertToUser(UserJoinRequest request) {
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+        Gender gender = Gender.valueOf(request.getGender());
+        return new User(request.getEmail(), encodedPassword, request.getNickname(),
+                gender, request.getAge());
+    }
+
+}
